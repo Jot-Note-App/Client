6111276b41041c352a016e710a4e9e7d3bef5e7b
Roy Dey <roydey10@gmail.com>
2023-11-15 01:13:18 -0800

    alter landing page styles

--- jot/src/App.tsx
+++ jot/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import './index.css'
 import GoogleLogout from './components/googleLogout';
 import SplashScreenBackground from './components/splashScreenBackground';
+import PenIcon from './icons/pen';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -12,24 +13,23 @@ function App() {
     <GoogleOAuthProvider clientId={clientId}>
       <SplashScreenBackground />
       <div className="flex min-h-screen">
-        <div className="ml-48 grid place-content-center gap-4">
-          <div className="text-center text-8xl font-bold">Jot</div>
-          <p className="text-slate-600 text-xl">
+        <div className="ml-48 grid place-content-center justify-items-center">
+          <div className="flex items-end mb-8 gap-1">
+            <div className="text-center text-8xl font-bold">Jot</div>
+            <PenIcon />
+          </div>
+          <p className="text-slate-500 text-xl mb-4">
             Notes with a pulse: Your sentiments, brilliantly organized
           </p>
-          <div className='grid justify-items-center'>
-            <GoogleLogin
-              onSuccess={credentialResponse => {
-                console.log(credentialResponse);
-                setIsLoggedIn(true);
-              }}
-              onError={() => {
-                console.log('Login Failed');
-              }}
-            />
-
-          </div>
-
+          <GoogleLogin
+            onSuccess={credentialResponse => {
+              console.log(credentialResponse);
+              setIsLoggedIn(true);
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          />
         </div>
       </div>
     </GoogleOAuthProvider >
