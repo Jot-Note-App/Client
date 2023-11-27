3a6f2fa3bb97a63765468d6867e403df72790efb
Roy Dey <roydey10@gmail.com>
2023-11-27 15:20:34 -0800

    install relay

--- jot/vite.config.ts
+++ jot/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import relay from "vite-plugin-relay";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [relay, react()],
+});
