6ed694e032f89ca9518524d1655f79ed6a57054f
Roy Dey <roydey10@gmail.com>
2024-02-13 21:57:23 -0800

    add query to main panel

--- jot/src/screens/MainPanel.tsx
+++ jot/src/screens/MainPanel.tsx
@@ -1,14 +1,60 @@
 import React from 'react';
 import { MainPanelTab } from '../enums/MainPanelTab';
+import { graphql, PayloadError, FragmentRef } from 'relay-runtime';
+import { useFragment, useLazyLoadQuery } from 'react-relay';
+import { useState, useEffect } from 'react';
 interface MainPanelProps {
     selectedTab: MainPanelTab;
 }
+// TODO: Implement infinite scroll pagination
+const journalSelectorFragment = graphql`
+  fragment MainPanelJournalSelectorFragment on User {
+    id
+    journals(first: 100){
+        edges {
+            node {
+                id
+                name
+            }
+        }
+    }
+  }
+`;
+
+const JournalSelector: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+const mainPanelQuery = graphql`
+query MainPanelQuery($entryId: ID){
+  user {
+    id
+    ...MainPanelJournalSelectorFragment
+  }
+}`;
 
 const MainPanel: React.FC<MainPanelProps> = ({ selectedTab }) => {
-
+    const [currEntryId, setCurrEntry] = useState<string | null>(null)
+    // const data = useLazyLoadQuery(mainPanelQuery, { currEntryId });
+    useEffect(() => {
+        const storedLastEntry = localStorage.getItem('lastEntryId');
+        if (storedLastEntry) {
+            setCurrEntry(storedLastEntry);
+        }
+    }, []);
+    useEffect(() => {
+        if (currEntryId) {
+            localStorage.setItem('lastEntryId', currEntryId)
+        }
+    }, [currEntryId])
     return (
-        <div className="">
-            {selectedTab}
+        <div className="grid grid-flow-row">
+            <JournalSelector />
+
         </div>
     );
 };
