4c31787787bf205af5d3ae3c4082d811a051dce1
Roy Dey <roydey10@gmail.com>
2024-03-09 17:03:37 -0800

    add custom tailwind styles

--- jot/tailwind.config.js
+++ jot/tailwind.config.js
@@ -9,8 +9,9 @@ export default {
       main: "#4C98FC",
       mainDark: "#146fe3",
       secondary: "#D6EBFF",
+      lightGray: '#E7E7E7',
+      mediumGray: "#BBBBBB",
       darkGray: "#828282",
-      lightGray: "#BBBBBB",
       offBlack: "#213547"
     },
     boxShadow: {
@@ -18,18 +19,20 @@ export default {
       md: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;',
       lg: 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;'
     },
-    // fontSize: {
-    //   h1: ["48px", { lineHeight: "56px" }],
-    //   h2: ["40px", { lineHeight: "46px" }],
-    //   h3: ["32px", { lineHeight: "37px" }],
-    //   h4: ["24px", { lineHeight: "35px" }],
-    //   h5: ["20px", { lineHeight: "30px" }],
-    //   large: ["16px", { lineHeight: "24px" }],
-    //   regular: ["14px", { lineHeight: "24px" }],
-    //   small: ["12px", { lineHeight: "20px" }],
-    //   xSmall: ["10px", { lineHeight: "16px" }],
-    // },
-    extend: {},
+    extend: {
+      fontSize: {
+        h1: ["48px", { lineHeight: "56px", fontWeight: "700" }],
+        h2: ["40px", { lineHeight: "46px", fontWeight: "700" }],
+        h3: ["32px", { lineHeight: "37px", fontWeight: "700" }],
+        h4: ["24px", { lineHeight: "35px", fontWeight: "700" }],
+        h5: ["20px", { lineHeight: "30px", fontWeight: "700" }],
+        subheading: ["17px", { lineHeight: "28px", fontWeight: "700" }],
+        large: ["16px", { lineHeight: "24px", fontWeight: "500" }],
+        regular: ["14px", { lineHeight: "24px", fontWeight: "500" }],
+        small: ["12px", { lineHeight: "20px", fontWeight: "500" }],
+        xSmall: ["11px", { lineHeight: "16px", fontWeight: "500" }],
+      }
+    },
   },
   plugins: [],
   purge: [
